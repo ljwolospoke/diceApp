ff0ba4acbc0486f310d7db3db5c37175533c09a1
ljwolospoke <35278976+ljwolospoke@users.noreply.github.com>
2019-04-22 18:28:08 -0400

    Add files via upload

---
+++ diceapp/diceapp/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  diceapp
+//
+//  Created by user152277 on 4/16/19.
+//  Copyright © 2019 user152277. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ diceapp/diceapp/Viewcontroller2.swift
@@ -0,0 +1,41 @@
+//
+//  ViewcontrollerViewController.swift
+//  diceapp
+//
+//  Created by Lance Woloszyn on 4/17/19.
+//  Copyright © 2019 Lance Woloszyn. All rights reserved.
+//
+
+import UIKit
+
+class ViewcontrollerViewController: UIViewController {
+ 
+    @IBAction func dFour(_ sender: Any) {
+        let number = Int.random(in: 0 ... 4)    }
+    @IBAction func dSix(_ sender: Any) {
+    }
+    @IBAction func dEight(_ sender: Any) {
+    }
+    @IBAction func dTen(_ sender: Any) {
+    }
+    @IBAction func dTwelve(_ sender: Any) {
+    }
+    @IBAction func dTwenty(_ sender: Any) {
+    }
+    @IBAction func dHundred(_ sender: Any) {
+    }
+    // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+
